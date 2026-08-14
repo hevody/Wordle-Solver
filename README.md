f6751ae4f6f5dba225b3fda4720fc8c41eef71fb
Velocity <145270377+hevody@users.noreply.github.com>
2026-08-15 06:29:15 +0800

    feat: new wordle solver feature
    I finally used regex for this

--- README.md
+++ README.md
@@ -4,4 +4,13 @@ Yeah, this is cheating, so I did not use it nor did I share it with my mom
 
 This project then had a use when I was passively listening to the radio whereas a radio station had a giveaway which offers free money of about 500 pesos if you solve the jumbled word
 <br>
-The word can be English, can be Tagalog
+The word can either be in English or in Tagalog
+
+# Features (Modes):
+## Wordle-like
+- I know the exact position of the letters but some letters are missing
+- I don't know the exact position of the letters and some letters are missing 
+
+# Roadmap
+- port into Discord
+- port into a Website
--- main-permutation.py
+++ main-permutation.py
@@ -1,4 +1,13 @@
 import copy
+import re
+import json
+
+### config ###
+with open('config.json') as f:
+    config = json.load(fp=f)
+
+dictionary = config['dictionary']
+### --- ###
 
 def permutations(potential_words_array: list) -> list:                        # side-note: although I use the terms word and letters, this can also apply to numbers
     ### variable declaration ###
@@ -17,8 +26,28 @@ def permutations(potential_words_array: list) -> list:                        # 
         listPoolCopy = copy.deepcopy(listPool)
     return permutated_list
 
-if __name__ == '__main__':
-    # Future feature: ??ilo
+def JUMBLE_search_in_dictionary(permutation_list: list, to_exact_length: int):
+    with open(dictionary) as f:
+        english = f.readlines()
+
+    word_w_exact_length = []
+    print('\n[RUNNING] dictionary check...')
+    for word_verbatim in english:
+        word_verbatim = word_verbatim.strip()
+        word = word_verbatim.lower()
+        word = word.strip()
+        if word in permutation_list:
+            print(f'[*] {word_verbatim}')
+            if len(word) == to_exact_length + 2:
+                word_w_exact_length += [word_verbatim]
+
+    print('\n[RUNNING] found words with exact length...')
+    for exact_word_candidate in word_w_exact_length:
+        print(f'[*] {exact_word_candidate}')
+
+def jumbled_letters_solver():
+    global listPool
+
     print('\nWhat are the jumbled letters?')
     print('Example: degeo')
     pool = input('\n> ')
@@ -31,22 +60,72 @@ if __name__ == '__main__':
     for _ in range(length):
         finalPerm += permutations(finalPerm)
 
-    with open('american-english') as f:
-        english = f.readlines()
+    JUMBLE_search_in_dictionary(finalPerm, to_exact_length=length)
 
-    word_w_exact_length = []
-    print('\n[RUNNING] dictionary check...')
-    for word_verbatim in english:
-        word_verbatim = word_verbatim.strip()
-        word = word_verbatim.lower()
-        word = word.strip()
-        if word in finalPerm:
-            print(f'[*] {word_verbatim}')
-            if len(word) == length + 2:
-                word_w_exact_length += [word_verbatim]
+def generate_a_regex_pattern(the_known_letters: list):
+    generated_pattern = r''
+    for letter in the_known_letters:
+        if letter == '?':
+            letter = '.'
+        generated_pattern += letter
+    return fr'^{generated_pattern}$'
 
-    print('\n[RUNNING] found word with exact length...')
-    for exact_word_candidate in word_w_exact_length:
-        print(f'[*] {exact_word_candidate}')
+def wordle_solver():
+    print('''\nExample:
+    known letters: ?e??e        
+    bad letters: cranmiht
+    clue letters: og
+    ''')
+
+    known_letters = list(input('known letters: '))       # use regex so finding the words in the dictionary will be easy
+    bad_letters = list(input('bad letters: '))
+    clue_letters = list(input('clue letters: '))                # this will be permutated, change of plans, no 
+
+    pattern = re.compile(fr'{generate_a_regex_pattern(known_letters)}')
+
+    matched_words = []
+    with open(dictionary) as f:
+        for line in f: 
+            word = line.strip()
+            if pattern.search(word):
+                matched_words += [word]
+
+    
+    list_contains_bad_letters = []
+    for letter in bad_letters:
+        for word in matched_words:
+            if letter in word:
+                list_contains_bad_letters += [word]
+            else:
+                continue
+
+    for word in list_contains_bad_letters:
+        try:
+            matched_words.remove(word)
+        except:
+            continue
+
+    possible_words = []
+    for word in matched_words:
+        word_array = list(word)
+        if all(item in word_array for item in clue_letters):
+            possible_words += [word]
+
+    print('\n[RUNNING] finding possible words...')
+    for possible_word in possible_words:
+        print(f'[*] {possible_word}')
+
+if __name__ == '__main__':
+    modes = {'wordle solver': wordle_solver, 
+             'jumbled letters solver': jumbled_letters_solver}
+    modes_list = list(modes.keys())
+
+    print('\nWhich MODE would you like to choose?')   
+    for mode in modes:
+        print(f'[{modes_list.index(mode) + 1}] {mode}')
+
+    chosen_mode = int(input('\n> ')) - 1
+    modes[modes_list[chosen_mode]]()
+    
 
 
